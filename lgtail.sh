06183bc45ea0740e0b78c8f23f1718ff89fb60d8
Гуськов Евгений Вячеславович <guskov@office.promit-ek.ru>
2024-06-10 08:23:14 +0500

    added installation script

--- lgtail.sh
+++ lgtail.sh
@@ -1,1 +1,1 @@
-luit -encoding cp1251 tail -f -F -n 40 /opt/MobileBooking/log/MKLog.$(date '+%Y-%m-%d').001.log
+luit -encoding cp1251 tail -f -F -n 40 /opt/MobileBooking/log/MKLog.$(date '+%Y-%m-%d').001.log;
---
+++ mk100_tools_install.sh
@@ -0,0 +1,3 @@
+export PATH=$PATH:/opt/asuppk_mk100_tools;
+
+echo 'PATH=$PATH:/opt/asuppk_mk100_tools' >> /root/.bashrc;
