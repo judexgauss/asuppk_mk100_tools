b8ed7a25b1dc589ffe7c6d618c4788425a3a6eb4
Guskov Evgenii <guskov@office.promit-ek.ru>
2024-06-10 09:09:56 +0500

    some corrections

--- mk100_tools_install.sh
+++ mk100_tools_install.sh
@@ -5,5 +5,4 @@ echo 'export PATH=$PATH:/opt/asuppk_mk100_tools' >> /root/.bashrc
 
 rm /opt/asuppk_mk100_tools/mk100_tools_install.sh
 
-echo "Installation done. Login again pls"
-exit
+echo "Installation done"
