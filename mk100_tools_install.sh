e7af617c770b4c1a279fdfb21524f5f68fd41e2e
Guskov Evgenii <guskov@office.promit-ek.ru>
2024-06-10 08:49:12 +0500

    removed ';'

--- mk100_tools_install.sh
+++ mk100_tools_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export PATH=$PATH:/opt/asuppk_mk100_tools;
+export PATH=$PATH:/opt/asuppk_mk100_tools
 
-echo 'export PATH=$PATH:/opt/asuppk_mk100_tools' >> /root/.bashrc;
+echo 'export PATH=$PATH:/opt/asuppk_mk100_tools' >> /root/.bashrc
 
-chmod +x /opt/asuppk_mk100_tools/*;
+chmod +x /opt/asuppk_mk100_tools/*
