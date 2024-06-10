5d46d2db5cd34f28350cfded540bf19a79c41cc1
Guskov Evgenii <guskov@office.promit-ek.ru>
2024-06-10 08:43:47 +0500

    added shebang

--- lgtail.sh
+++ lgtail.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 luit -encoding cp1251 tail -f -F -n 40 /opt/MobileBooking/log/MKLog.$(date '+%Y-%m-%d').001.log;
--- mk100_tools_install.sh
+++ mk100_tools_install.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
 export PATH=$PATH:/opt/asuppk_mk100_tools;
 
 echo 'PATH=$PATH:/opt/asuppk_mk100_tools' >> /root/.bashrc;
+
+chmod +x /opt/asuppk_mk100_tools/*;
