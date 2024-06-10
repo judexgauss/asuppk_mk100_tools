61e255d8552a66be74baa95b70cec03cc670ad1c
Guskov Evgenii <guskov@office.promit-ek.ru>
2024-06-10 09:06:57 +0500

    added deleting install.sh

--- mk100_tools_install.sh
+++ mk100_tools_install.sh
@@ -3,4 +3,7 @@
 echo >> /root/.bashrc
 echo 'export PATH=$PATH:/opt/asuppk_mk100_tools' >> /root/.bashrc
 
+rm /opt/asuppk_mk100_tools/mk100_tools_install.sh
+
 echo "Installation done. Login again pls"
+exit
