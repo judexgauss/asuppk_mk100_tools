c23d4a6e783e27847778908ea75455ece40a0f02
Guskov Evgenii <guskov@office.promit-ek.ru>
2024-06-10 08:48:04 +0500

    corrected export of env var

--- lgtail.sh
+++ lgtail.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+
 luit -encoding cp1251 tail -f -F -n 40 /opt/MobileBooking/log/MKLog.$(date '+%Y-%m-%d').001.log;
--- mk100_tools_install.sh
+++ mk100_tools_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
 export PATH=$PATH:/opt/asuppk_mk100_tools;
 
-echo 'PATH=$PATH:/opt/asuppk_mk100_tools' >> /root/.bashrc;
+echo 'export PATH=$PATH:/opt/asuppk_mk100_tools' >> /root/.bashrc;
 
 chmod +x /opt/asuppk_mk100_tools/*;
